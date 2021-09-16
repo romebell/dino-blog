4c9bfc96a880e72f21de4fcacfc254c7f846a2eb
Rome Bell <romebell5@gmail.com>
2021-09-16 10:00:55 -0700

    add: App, Comment, Dino

---
+++ src/Dino.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Comment from './Comment.js';
+
+function Dino(props) {
+    console.log(props.name);
+    let allComments = props.comments.map(function(c, i){
+        return <Comment key={i} body={c}/>
+    });
+
+    return (
+        <div>
+            <h1>{props.title}!</h1>
+            <p>{props.author}</p>
+            <p>{props.body}</p>
+            <div className="com">
+                <h3>Comments:</h3>
+                {allComments}
+            </div>
+        </div>
+    );
+}
+
+export default Dino;
